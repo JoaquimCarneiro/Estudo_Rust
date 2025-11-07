a5f047919b4d222013ff3a42238d4e7e37196f34
Joaquim Carneiro <Joaquimadeira@gmail.com>
2025-11-07 15:18:32 +0000

    add rand

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
+    let secret = rand::thread_rng().gen_range(1..=100);
     println!("Guessing Game!");
     println!("Insert a number: ");
 
     let mut guess = String::new();
+    println!("Secret: {secret}");
 
     io::stdin()
         .read_line(&mut guess)
