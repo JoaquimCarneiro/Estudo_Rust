b8ba4131465263ab1e7a8dc9043ae259510ef28a
Joaquim Madeira <joaquimadeira@gmail.com>
2025-11-09 22:15:10 +0000

    adicionar ciclo para multiplas hipoteses e condição de saída deste ciclo.

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,28 @@ use rand::Rng;
 
 fn main() {
     let secret = rand::thread_rng().gen_range(1..=100);
-    println!("Guessing Game!");
-    println!("Insert a number: ");
 
-    let mut guess = String::new();
+    println!("Guessing Game!");
     println!("Secret: {secret}");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Insert a number: ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Not a number !");
+        let guess: u32 = guess.trim().parse().expect("Not a number !");
 
-    println!("your guess: {guess}");
+        println!("your guess: {guess}");
 
-    match guess.cmp(&secret) {
-        Ordering::Less => println!("Too smsll"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
