b20946532afc6ba246e845e61400c4b559f84506
Joaquim Madeira <joaquimadeira@gmail.com>
2025-11-09 22:27:25 +0000

    gerir entrada de dados inválida

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Not a number !");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("your guess: {guess}");
 
