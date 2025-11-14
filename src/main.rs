866f69505a2f015d5cbbce7f00aa4099331f64e8
Joaquim Carneiro <Joaquimadeira@gmail.com>
2025-11-14 21:11:43 +0000

    part 3.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("not hello world");
+    // Variables are imutable by default
+    let y = 10;
+    // y = 15; //thi causes a compile error.
+    println!("The value of Y is {y}, changing this value is not permited.");
+    // mutable variable
+    let mut x = 5;
+    println!("The value of X is {x}");
+    x = 6;
+    println!("and now X is {x}");
 }
