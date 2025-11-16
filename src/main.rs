da20352658fbc7737f9a665bcce8d63135c490f2
Joaquim Carneiro <Joaquimadeira@gmail.com>
2025-11-16 10:54:13 +0000

    typos

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 fn main() {
     // Variables are imutable by default
     let y = 10;
-    // y = 15; //thi causes a compile error.
+    // y = 15; //this causes a compile error.
     println!("The value of Y is {y}, changing this value is not permited.");
     // mutable variable
     let mut x = 5;
     println!("The value of X is {x}");
     x = 6;
     println!("and now X is {x}");
+    // constants
+    // can't be changed - always immutable
+    // cant't use mut
+    // type must be annotated
 }
